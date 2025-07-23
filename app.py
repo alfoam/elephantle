2840076be1fc28c09ea49f02e088ecf4abb26ff6
alfoam <adamsun20003@gmail.com>
2025-07-23 04:43:02 -0400

    added scheduler

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask_cors import CORS
 
 import json
@@ -11,7 +12,16 @@ with open("data.json", "r") as f:
     DICT = json.load(f)
 
 #CORRECT = "Phosphatherium"
-CORRECT = random.choice(list(DICT))
+CORRECT = None
+
+def pick_new_correct():
+    global CORRECT
+    CORRECT = random.choice(list(DICT))
+    print(f"New CORRECT selected: {CORRECT}")
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=pick_new_correct, trigger="interval", hours=24)
+scheduler.start()
 
 @app.route("/data")
 def get_data():
